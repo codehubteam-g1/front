0caaf59233581f876e23d4e34199819e52bc4dcd
Sergio Álvarez Gómez <sf.alvarez10@uniandes.edu.co>
2019-07-29 08:41:26 -0500

    Improved the application of the .navbar style

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div id="background-container" class="has-navbar-fixed-top">
     <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
     <nav
-      class="navbar navbar-brand"
+      class="navbar"
       role="navigation"
       aria-label="main navigation"
       style="border-bottom: 1px solid #e6e6e6;"
@@ -261,7 +261,8 @@ export default {
     overflow-y: scroll;
 } */
 
-.navbar-brand {
+.navbar {
   min-height: 6.25em; /* Increases the minimun height of the navbar from 3.25em to 6.25em */
 }
+
 </style>
