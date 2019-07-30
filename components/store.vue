e75ce0c58c199803ace4f26895f0a91bba3a336c
Sergio Álvarez Gómez <sf.alvarez10@uniandes.edu.co>
2019-07-30 02:59:58 -0500

    Implemented the use of props in order to easily pass data from the parent component 'stores' to the child component 'store'

--- components/store.vue
+++ components/store.vue
@@ -1,39 +1,41 @@
 <template>
-  <div class="card" style="margin: 20px 0px 20px 0px">
-    <div class="card-image">
-      <figure class="image is-2by1">
-        <img
-          src="https://cdn.vox-cdn.com/thumbor/w4Ddhb6pjvkU2d1vGPWoJpqOU0Y=/0x0:7425x6750/1200x800/filters:focal(3119x2781:4307x3969)/cdn.vox-cdn.com/uploads/chorus_image/image/63654502/SouthFlorida_InternationalMenuItems.0.jpg"
-          alt="Placeholder image"
-        />
-      </figure>
-    </div>
-
-    <div class="card-content">
-
-      <div class="media">
-        <div class="media-content">
-          <p class="title is-4">Mc Donald's Chapinero</p>
-          <p class="subtitle is-6">Carrera 4A #55-55</p>
-        </div>
-
-        <div class="media-right">
-          <figure class="image is-64x64">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/5/50/McDonald%27s_SVG_logo.svg"
-              alt="Placeholder image"
-            />
-          </figure>
-        </div>
+  <a href>
+    <div class="card" style="margin: 20px 0px 20px 0px">
+      <div class="card-image">
+        <figure class="image is-2by1">
+          <img v-bind:src="storeData.coverPictureUrl" />
+        </figure>
       </div>
 
-      <div class="content">
-        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-        <div class="stars">4.5 e</div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{{storeData.name}}</p>
+            <p class="subtitle is-6 has-text-grey">{{storeData.description}}</p>
+          </div>
+
+          <div class="media-right">
+            <figure class="image is-64x64">
+              <img v-bind:src="storeData.logoUrl" />
+            </figure>
+          </div>
+        </div>
+
+        <div class="content">
+          <div class="rectangle">
+            <span>30 - 40 min</span>
+          </div>
+          <div class="rectangle" style="has-icons-right">
+            <span>4.3</span>
+            <span class="icon is-small is-right" style="color: orange">
+              <i class="fas fa-star"></i>
+            </span>
+            <span class="has-text-grey">(500+)</span>
+          </div>
+        </div>
       </div>
-      
     </div>
-  </div>
+  </a>
 </template>
 
 <script>
@@ -41,42 +43,19 @@ import axios from "axios";
 
 export default {
   data() {
-    return {};
+    return {
+    };
   },
-  methods: {
-    // update() {
-    //   let token = this.$auth.getToken("local");
-    //   this.$axios
-    //     .get("http://127.0.0.1:3001/users", { Authorization: "bearer" + token })
-    //     .then(response => {
-    //       let pictureUrl = response.data.user.profilePictureUrl;
-    //       if (pictureUrl) this.profilePictureUrl = pictureUrl;
-    //       else this.profilePictureUrl = this.profilePictureUrlDefault;
-    //     })
-    //     .catch(error => error);
-    // }
-  },
-
-  computed: {}
-
-  // created: function() {
-  //   let token = this.$auth.getToken("local");
-  //   this.$axios
-  //     .get("http://127.0.0.1:3001/users", { Authorization: "bearer" + token })
-  //     .then(response => {
-  //       let pictureUrl = response.data.user.profilePictureUrl;
-  //       if (pictureUrl) this.profilePictureUrl = pictureUrl;
-  //       else this.profilePictureUrl = this.profilePictureUrlDefault;
-  //     })
-  //     .catch(error => error);
-  // }
+  props: [
+    'storeData'
+  ]
 };
 </script>
 
 <style scoped>
-  .stars {
-    display: inline-block;
-    background-color: rgb(203, 202, 204);
-    padding: 5px 15px 5px 15px
-  }
+.rectangle {
+  display: inline-block;
+  background-color: #f5f5f5;
+  padding: 5px 15px 5px 15px;
+}
 </style>
--- components/stores.vue
+++ components/stores.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="columns" style="padding: 6.25em 40px 0px 40px">
-      <div class="column">
-          <store/>
-          <store/>
-          <store/>
-      </div>
-      <div class="column">
-          <store/>
-          <store/>
-          <store/>
-      </div>
-      <div class="column">
-          <store/>
-          <store/>
-          <store/>
-      </div>
+    <div class="column">
+      <store :storeData="prueba" />
+    </div>
+    <div class="column"></div>
+    <div class="column"></div>
   </div>
 </template>
 
 <script>
-import store from "~/components/store"
+import store from "~/components/store";
 export default {
-    components: {
-        store
-    }
+  data() {
+    return {
+        prueba: {
+            name: 'Restorando',
+            description: 'Pizzas',
+            coverPictureUrl: 'http://localhost:3000/_nuxt/static/rappi-logo.svg',
+            logoUrl: 'http://localhost:3000/_nuxt/static/rappi-logo.svg'
+        }
+    };
+  },
+  components: {
+    store
+  }
 };
 </script>
 
