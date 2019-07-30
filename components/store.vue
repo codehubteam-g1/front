54ff477128aacedc7484b52dbc01b527532b7a2f
Sergio Álvarez Gómez <sf.alvarez10@uniandes.edu.co>
2019-07-30 08:25:17 -0500

    Small visual change that aligns the left side of all containers text

--- components/store.vue
+++ components/store.vue
@@ -1,13 +1,13 @@
 <template>
   <a href>
-    <div class="card" style="margin: 20px 0px 20px 0px">
+    <div class="card" style="margin: 20px 0px 20px 0px;">
       <div class="card-image">
         <figure class="image is-2by1">
           <img v-bind:src="store.coverPictureUrl" />
         </figure>
       </div>
 
-      <div class="card-content">
+      <div class="card-content" style="padding-left: 30px">
         <div class="media">
           <div class="media-content">
             <p class="title is-4">{{store.name}}</p>
@@ -22,15 +22,15 @@
         </div>
 
         <div class="content">
-          <div class="rectangle">
-            <span>30 - 40 min</span>
-          </div>
-          <div class="rectangle" style="has-icons-right">
+          <div class="rectangle" style="has-icons-right; margin-left: -15px">
             <span>{{score}}</span>
             <span class="icon is-small is-right" style="color: orange">
               <i class="fas fa-star"></i>
             </span>
             <span class="has-text-grey">({{number}}+)</span>
+          </div>
+          <div class="rectangle">
+            <span>30 - 40 min</span>
           </div>
         </div>
       </div>
@@ -44,13 +44,11 @@ import axios from "axios";
 export default {
   data() {
     return {
-      score: (Math.random()+3.6).toFixed(1),
-      number: Math.ceil(Math.random()*10)*50
+      score: (Math.random() + 3.6).toFixed(1),
+      number: Math.ceil(Math.random() * 10) * 50
     };
   },
-  props: [
-    'store'
-  ]
+  props: ["store"]
 };
 </script>
 
