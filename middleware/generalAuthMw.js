4b10e49bd4d7b64d4ab3b73bf8376e3e6bf0cd3f
Sergio Álvarez <sf.alvarez11@gmail.com>
2020-01-27 14:25:39 +0000

    Fixed bad route in middleware that was pointing to localhost instead of the api

--- middleware/generalAuthMw.js
+++ middleware/generalAuthMw.js
@@ -2,6 +2,6 @@ export default function(context) {
     console.log(context.$auth.loggedIn)
      console.log(context.$auth.user)
     if(!context.$auth.loggedIn){
-      context.redirect('localhost:3000/user/login');   
+      context.redirect(process.env.apiUrl+'/user/login');   
     }
 }
